b00f05778afcf6413625182dcdae9301c59a03c3
progriro <progriro@gmail.com>
2021-02-23 00:31:55 +0900

    [created] src/components/atoms/Title.tsx

---
+++ src/components/atoms/Title.tsx
@@ -0,0 +1,16 @@
+export const config = { amp: true };
+
+export const Title: React.FC = ({ children }) => {
+  return (
+    <>
+      <h1>{children}</h1>
+      <style jsx>
+        {`
+          h1 {
+            font-size: 40px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
