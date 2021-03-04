30c85bac7372255829a185789a840d7438be0123
progriro <progriro@gmail.com>
2021-03-04 22:15:56 +0900

    [modified] Hero.tsxのデザインを変更

--- src/components/atoms/Hero.tsx
+++ src/components/atoms/Hero.tsx
@@ -7,9 +7,27 @@ export const Hero: React.FC = () => {
       <style jsx>
         {`
           h1 {
-            font-size: 60px;
-            text-align: center;
-            border: solid 5px black;
+            margin: 2em 0;
+            position: relative;
+            padding: 0.5em 1.5em;
+            border-top: solid 2px black;
+            border-bottom: solid 2px black;
+          }
+          h1:before,
+          h1:after {
+            content: "";
+            position: absolute;
+            top: -10px;
+            width: 2px;
+            height: -webkit-calc(100% + 20px);
+            height: calc(100% + 20px);
+            background-color: black;
+          }
+          h1:before {
+            left: 10px;
+          }
+          h1:after {
+            right: 10px;
           }
         `}
       </style>
