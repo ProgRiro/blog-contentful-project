172d442b180808339bde1015f8ff430a3058ebf8
progriro <progriro@gmail.com>
2021-03-05 16:10:31 +0900

    [add] remark-gfmプラグインを追加

--- src/components/organisms/PostContent.tsx
+++ src/components/organisms/PostContent.tsx
@@ -1,9 +1,10 @@
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 export const PostContent = ({ content }: any) => {
   return (
     <div>
-      <ReactMarkdown source={content} />
+      <ReactMarkdown plugins={[gfm]} source={content} />
     </div>
   );
 };
