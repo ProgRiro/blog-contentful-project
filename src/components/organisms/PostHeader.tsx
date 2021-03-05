ecc801f201d8186338972b47a21a790994d19921
progriro <progriro@gmail.com>
2021-03-05 18:00:39 +0900

    [created] PostHeader.tsx

---
+++ src/components/organisms/PostHeader.tsx
@@ -0,0 +1,93 @@
+export const config = { amp: true };
+
+import { AuthorIntroduce } from ".";
+
+interface Props {
+  title: string;
+  date: string;
+  coverImageUrl: string;
+  authorName: string;
+  authorPictureUrl: string;
+}
+
+export const PostHeader: React.FC<Props> = ({
+  title,
+  date,
+  coverImageUrl,
+  authorName,
+  authorPictureUrl,
+}) => {
+  const published = new Date(date);
+  return (
+    <div>
+      <div className="header">
+        <div className="headerLeft">
+          <div className="titleAndPublished">
+            <h1 className="title">{title}</h1>
+            <amp-timeago
+              height="30"
+              width="200"
+              layout="fixed"
+              datetime={date}
+              locale="en"
+            >
+              {published.toLocaleDateString()}
+            </amp-timeago>
+          </div>
+          <amp-img
+            width="160"
+            height="90"
+            src={coverImageUrl}
+            layout="responsive"
+            alt={`${title}-cover-image`}
+          />
+        </div>
+        <div className="headerRight">
+          <AuthorIntroduce
+            authorName={authorName}
+            authorPictureUrl={authorPictureUrl}
+          />
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .header {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+          }
+          .titleAndPublished {
+            text-align: center;
+            margin-bottom: 20px;
+          }
+          .title {
+            font-size: 35px;
+            margin: 0;
+          }
+          .headerLeft {
+            width: 70%;
+          }
+          .headerRight {
+            width: 22%;
+          }
+          @media screen and (max-width: 1024px) {
+            .headerLeft {
+              width: 100%;
+            }
+            .headerRight {
+              display: none;
+            }
+          }
+          @media screen and (max-width: 480px) {
+            .headerLeft {
+              width: 100%;
+            }
+            .headerRight {
+              display: none;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,4 +2,6 @@ export const config = { amp: true };
 export * from "./PostCard";
 export * from "./Footer";
 export * from "./Meta";
-export * from "./PostContent";
+export * from "./PostHeader";
+export * from "./PostBody";
+export * from "./AuthorIntroduce";
