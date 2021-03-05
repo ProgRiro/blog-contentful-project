c8afed8a5d5bf1c021165720c9d7ae1ba5d25658
progriro <progriro@gmail.com>
2021-03-05 10:14:18 +0900

    [modified] Homeをレスポンシブに対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,16 @@ export const config = { amp: true };
 import Head from "next/head";
 
 import { PageLayout } from "@/components/templates";
-import { Avatar, AuthorName } from "@/components/atoms";
-import { Author } from "@/components/molecules";
 import { PostCard } from "@/components/organisms";
+import { CMS_NAME } from "@/lib/constants";
 
 export default function Home() {
   return (
     <PageLayout>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{CMS_NAME}</title>
       </Head>
-      <div className="gridPostsContainer_column2">
+      <div className="gridPostsContainer">
         <PostCard />
         <PostCard />
         <PostCard />
@@ -29,10 +27,20 @@ export default function Home() {
           body {
             background-color: gray;
           }
-          .gridPostsContainer_column2 {
+          .gridPostsContainer {
             display: grid;
             grid-template-columns: repeat(3, 1fr);
             grid-gap: 2em 1.8em;
+          }
+          @media screen and (max-width: 1024px) {
+            .gridPostsContainer {
+              grid-template-columns: repeat(2, 1fr);
+            }
+          }
+          @media screen and (max-width: 480px) {
+            .gridPostsContainer {
+              grid-template-columns: repeat(1, 1fr);
+            }
           }
         `}
       </style>
