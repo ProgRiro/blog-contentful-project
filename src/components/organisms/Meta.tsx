9b9aa1d849feb27b42d13a839de6a48c187f234e
progriro <progriro@gmail.com>
2021-03-07 13:33:01 +0900

    [add] fonts to Meta.tsx

--- src/components/organisms/Meta.tsx
+++ src/components/organisms/Meta.tsx
@@ -38,8 +38,14 @@ export const Meta = () => {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-      <style amp-custom>{`
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&family=Noto+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <style jsx global>{`
         body {
+          font-family: "Noto Sans", "M PLUS Rounded 1c", sans-serif;
           background-color: #f2efe4;
         }
       `}</style>
