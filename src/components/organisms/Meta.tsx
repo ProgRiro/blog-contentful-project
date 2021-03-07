b94390484242a9d2f335ba624e2cb1160bbab7c4
progriro <progriro@gmail.com>
2021-03-08 02:07:48 +0900

    [modified] Meta.tsx

--- src/components/organisms/Meta.tsx
+++ src/components/organisms/Meta.tsx
@@ -6,6 +6,7 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 export const Meta = () => {
   return (
     <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       {/* <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -36,15 +37,14 @@ export const Meta = () => {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&family=Noto+Sans:wght@400;700&display=swap"
         rel="stylesheet"
       />
       <style jsx global>{`
         body {
-          font-family: "Noto Sans", "M PLUS Rounded 1c", sans-serif;
           background-color: #f2efe4;
+          font-family: "Noto Sans", "M PLUS Rounded 1c", sans-serif;
         }
       `}</style>
     </Head>
