73bf40d81565fc1417438a73665b1c767e2239fb
progriro <progriro@gmail.com>
2021-03-07 17:11:50 +0900

    [modified] meta情報をdocument.tsxに記述

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
 import type { AppProps } from "next/app";
 
-import { Meta } from "@/components/organisms";
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Meta />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000" />
+          <meta
+            name="description"
+            content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+          />
+          <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&family=Noto+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+          <style jsx global>{`
+            body {
+              font-family: "Noto Sans", "M PLUS Rounded 1c", sans-serif;
+              background-color: #f2efe4;
+            }
+          `}</style>
+        </Head>
         <body>
           <Main />
           <NextScript />
